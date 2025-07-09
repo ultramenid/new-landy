34cb9129d9b53acafcaef8e13b0bb72d8b7db942
ultramenid <malichamdan@gmail.com>
2025-07-09 18:26:54 +0700

    local logos and fix footer nav

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,34 +5,34 @@
                     <h1>Co-creators</h1>
                     <div class="flex justify-between w-full flex-wrap">
                         <a href="https://auriga.or.id/" target="_blank" >
-                            <img class="h-10 mt-2" src="https://mapbiomas.nusantara.earth/assets/images/AurigaPNG.png" alt="">
+                            <img class="h-10 mt-2" src="{{ asset('assets/logos/AurigaPNG.png') }}" alt="">
                         </a>
                         <a href="https://www.jeratpapua.org/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo2_color.png" alt="" class="h-12 ">
+                            <img src="{{ asset('assets/logos/logo2_color.png') }}" alt="" class="h-12 ">
                         </a>
                         <a href="https://saveourborneo.org/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo3_color.png" alt="" class="h-12">
+                            <img src="{{ asset('assets/logos/logo3_color.png') }}" alt="" class="h-12">
                         </a>
                         <a href="http://greenofborneo.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo4_color.png" alt="" class="h-12">
+                            <img src="{{ asset('assets/logos/logo4_color.png') }}" alt="" class="h-12">
                         </a>
                         <a href="https://www.haka.or.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo5-x.png" alt="" class="h-12">
+                            <img src="{{ asset('assets/logos/logo5-x.png') }}" alt="" class="h-12">
                         </a>
                         <a href="http://mnukwarpapua.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo6-x.png" alt="" class="h-12">
+                            <img src="{{ asset('assets/logos/logo6-x.png') }}" alt="" class="h-12">
                         </a>
                         <a href="https://hutaninstitute.or.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo7-x.png" alt="" class="h-12">
+                            <img src="{{ asset('assets/logos/logo7-x.png') }}" alt="" class="h-12">
                         </a>
                         <a href="http://yayasangenesisbengkulu.or.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo8-x.png" alt="" class="h-12">
+                            <img src="{{ asset('assets/logos/logo8-x.png') }}" alt="" class="h-12">
                         </a>
                          <a href="https://komiu.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo9-x.png" alt="" class="h-12">
+                            <img src="{{ asset('assets/logos/logo9-x.png') }}"  alt="" class="h-12">
                         </a>
                         <a href="http://sampankalimantan.id/" target="_blank" >
-                            <img src="https://mapbiomas.nusantara.earth/assets/images/logo10-x.png" alt="" class="h-12">
+                            <img src="{{ asset('assets/logos/logo10-x.png') }}" alt="" class="h-12">
                         </a>
 
                     </div>
@@ -42,7 +42,7 @@
                 <div class="sm:w-2/12 w-full">
                     <h1 class="">Supported by</h1>
                     <a href="https://woods-wayside.org/" target="_blank" class="">
-                        <img src="https://mapbiomas.nusantara.earth/assets/images/logo12-x.png" alt="" class="h-12">
+                        <img src="{{ asset('assets/logos/logo12-x.png') }}" alt="" class="h-12">
                     </a>
                 </div>
 
@@ -56,7 +56,7 @@
  <div class="bg-gray-500 border-t-4 border-landy mt-20 ">
     <div class="flex sm:flex-row flex-col gap-8  py-4 mt-4 max-w-6xl mx-auto px-4">
         <div class="flex flex-col">
-            <a href="{{ route('about', app()->getlocale() )}}" class="text-white">about</a>
+            <a href="{{ route('about', app()->getlocale() )}}" class="text-white">{{__('about') }}</a>
 
         </div>
         <div class="flex flex-col">
@@ -64,23 +64,28 @@
         </div>
 
         <div class="flex flex-col">
-            <a class="text-white">map & data</a>
-            <a href="{{ route('termsofuse', app()->getlocale() )}}" class=" text-white">term of use</a>
-            <a href="https://platform.indonesia.mapbiomas.org/fogo" class="text-white">platform & map</a>
+            <a class="text-white">{{__('map & data') }}</a>
+            <a href="{{ route('termsofuse', app()->getlocale() )}}" class=" text-white text-sm mt-2">{{__('terms of use') }}</a>
+            <a href="https://platform.indonesia.mapbiomas.org/fogo" class="text-white text-sm">{{__('platform/map') }}</a>
+            <a href="{{ route('statistics', app()->getlocale() )}}" class="text-white text-sm">{{__('statistics') }}</a>
+            <a href="{{ route('accuracy-assessment', app()->getlocale() )}}" class="text-white text-sm">{{__('accuracy assessment') }}</a>
+            <a href="{{ route('infographics', app()->getlocale() )}}" class="text-white text-sm">{{__('infographics') }}</a>
         </div>
 
         <div class="flex flex-col">
             <a class="text-white">methodology</a>
-            <a href="{{ route('atbd', app()->getlocale() )}}" class="text-white ">ATBD</a>
-            <a href="{{ route('refrencemap', app()->getlocale() )}}" class="text-white">reference map</a>
+            <a href="{{ route('atbd', app()->getlocale() )}}" class="text-white text-sm mt-2">ATBD</a>
+            <a href="{{ route('refmap', app()->getlocale() )}}" class="text-white text-sm">{{__('references map') }}</a>
+            <a href="{{ route('gee', app()->getlocale() )}}" class="text-white text-sm">google earth engine</a>
+            <a href="{{ route('glossary', app()->getlocale() )}}" class="text-white text-sm">{{__('glossary') }}</a>
         </div>
 
         <div class="flex flex-col">
-            <a class="text-white">news & event</a>
+            <a href="{{ route('newsnevent', app()->getlocale() )}}" class="text-white">{{__('news & event') }}</a>
         </div>
 
         <div class="flex flex-col">
-            <a href="{{ route('downloads', app()->getlocale() )}}" class="text-white" class="font-semibold">downloads</a>
+            <a href="{{ route('downloads', app()->getlocale() )}}" class="text-white" class="font-semibold">{{__('downloads') }}</a>
         </div>
 
     </div>
